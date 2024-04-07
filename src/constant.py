8f3b5273318e53c75e4fae6cd5743b81333f1b7f
jaeseo <jaeserrr@ewhain.net>
2024-04-07 19:09:06 +0900

    [ADD] result xml folder

--- src/constant.py
+++ src/constant.py
@@ -2,6 +2,7 @@
 DATA_PATH = f"../data"
 MODEL_PATH = f"../model"
 IMAGE_PATH = f"../images/"
+RESULT_XML_PATH = f"../result-xml/"
 
 DATA_FEATURE_PATH = f"{DATA_PATH}/processed-feature"
 DATA_RAW_PATH = f"{DATA_PATH}/raw"
